cd6cd42ded784a84ba758b4430874996a0e5bad9
Miguel Angel Alarcos Torrecillas <miguelangel.alarcos@proton.me>
2025-05-25 18:16:59 +0200

    mappings telescope trouble

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+            ["<leader>t1"] = { ":ToggleTerm 1<CR>", desc = "Open terminal 1"},
+            ["<leader>t2"] = { ":ToggleTerm 2<CR>", desc = "Open terminal 2"},
+       },
+      },
+    },
+  },
+}
