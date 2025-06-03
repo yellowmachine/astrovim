2fae8134611effda75abd3dcc66abea1167b0a8b
miguel <miguelangel.alarcos@proton.me>
2025-06-03 06:40:33 +0200

    database plugin

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -8,6 +8,7 @@ return {
         n = {
           ["<leader>t1"] = { ":ToggleTerm 1<CR>", desc = "Open terminal 1" },
           ["<leader>t2"] = { ":ToggleTerm 2<CR>", desc = "Open terminal 2" },
+          ["<leader>t3"] = { ":ToggleTerm 3<CR>", desc = "Open terminal 3" },
           --["<leader>as"] = { ":Autosave toggle<CR>", desc = "Toggle autosave" },
         },
       },
